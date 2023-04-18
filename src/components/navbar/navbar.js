87bfcceead9e958cbfea8fe054d82cb5511fc96b
Luxe17 <hanson1717@live.com>
2023-04-18 09:17:55 -0600

    Project setup

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar(){
+    return<>
+        <header>
+            <h1>Find Flix</h1>
+            <ul>
+                <li>Sign In</li>
+                <li>Create Account</li>
+                <li>Films</li>
+                <li>Lists</li>
+                <li>Members</li>
+                <li>Journal</li>
+            </ul>
+            <form>
+                <input type="text"></input>
+            </form>
+        </header>
+    </>
+}
